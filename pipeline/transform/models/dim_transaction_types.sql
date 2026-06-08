498a4a55a5d7d317a920c7edc7fdc337630ff79e
ibrahimnassar24 <ebrahim24111990@gmail.com>
2026-06-08 11:40:34 +0300

    create the models folder and file structure

--- pipeline/transform/models/dim_transaction_types.sql
+++
@@ -1,8 +0,0 @@
-with transaction_types as (
-    select transaction_type
-    from {{ source('finance_raw', 'transactions') }}
-    group by transaction_type
-)
-
-select gen_random_uuid() as id, transaction_type as transaction_types
-from transaction_types

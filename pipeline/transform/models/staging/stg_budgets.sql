b17771a8612df7618a6f66521de305b14c534b56
Ibrahim Khaled <ebrahim24111990@gmail.com>
2026-06-16 14:44:34 +0300

    update stg_budget

--- pipeline/transform/models/staging/stg_budgets.sql
+++ pipeline/transform/models/staging/stg_budgets.sql
@@ -4,19 +4,31 @@ with source as (
 
 ),
 
+limits as (
+
+    select * from {{ source('raw', 'budget_limits') }}
+
+),
+
 renamed as (
 
     select
-        id as budget_id,
-        user_id,
-        name as budget_name,
-        active as is_active,
-        encrypted as is_encrypted,
-        created_at,
-        updated_at,
-        deleted_at
+        s.id as budget_id,
+        s.user_id,
+        s.name as budget_name,
+        s.active as is_active,
+        s.encrypted as is_encrypted,
+        s.created_at,
+        s.updated_at,
+        s.deleted_at,
+        l.start_date as limit_start_date,
+        l.end_date as limit_end_date,
+        l.amount as limit_amount,
+        l.transaction_currency_id
 
-    from source
+    from source s
+    left join limits l
+    on s.id = l.budget_id
 
 )
 
